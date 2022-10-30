8afdb3914b6d1704340a49d179d30cbbfdf625da
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-10-30 05:16:47 +0100

    added 2 pictures (Zuri and I4G logos) to the assets folder and rendered them on the footer section, styled the footer section.

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Footer.css";
+import zuriLogo from "../../assets/Zuri.Internship_Logo.webp";
+import I4GLogo from "../../assets/I4G.webp";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <>
+      <img src={zuriLogo} alt="" />
+      <p className="footer-text">HNG Internship 9 Frontend Task</p>
+      <img src={I4GLogo} alt="" />
+    </>
   );
 }
 
