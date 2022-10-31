a17a39e1276f696b91c351c67a675b05b435771d
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-10-31 07:19:44 +0100

    added 1 picture to the assets folder, implemented image overlay on the profile picture.

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./Profile.css";
 import profilePicture from "../../assets/profile__img.webp";
+import camera from "../../assets/camera__icon.webp";
 
 function Profile() {
   return (
     <>
-      <img
-        src={profilePicture}
-        alt=""
-        id="profile__img"
-        className="profile-picture"
-      />
+      <div className="picture-container">
+        <img
+          src={profilePicture}
+          alt=""
+          id="profile__img"
+          className="profile-picture"
+        />
+        <div className="overlay fadeEffect">
+          <img
+            src={camera}
+            alt=""
+            id="profile__img"
+            className="profile-picture"
+          />
+        </div>
+      </div>
       <p id="twitter" className="twitter-username">
         aza_jenny25
       </p>
