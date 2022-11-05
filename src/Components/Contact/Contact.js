327b4fed6bc7a15b086df4db6508af17a1ec01ac
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-05 10:43:15 +0100

    refactor: Added 3 input fields (first name, last name and email)and 1 textarea field, each with the appropriate id.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,37 @@ function Contact() {
     <div className="contact-me">
       <h2>Contact Me</h2>
       <p>Hi there, contact me to ask me about anything you have in mind.</p>
-      <form></form>
+      <form>
+        <section className="name-fields">
+          <div className="field">
+            <label htmlFor="first_name">First name</label>
+            <input
+              id="first_name"
+              type="text"
+              placeholder="Enter your first name"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="last_name">Last name</label>
+            <input
+              id="last_name"
+              type="text"
+              placeholder="Enter your last name"
+            />
+          </div>
+        </section>
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" placeholder="yourname@email.com" />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            placeholder="Send me a message and I'll reply you as soon as possible..."
+          />
+        </div>
+      </form>
     </div>
   );
 }
