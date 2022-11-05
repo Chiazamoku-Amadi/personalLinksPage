0125cb39df623810f0281180733e6dd1b94ed42b
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-05 09:18:37 +0100

    refactor: Added title and sub-text to the contact page, imported Contact.css.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact-me">
+      <h2>Contact Me</h2>
+      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      <form></form>
     </div>
   );
 }
