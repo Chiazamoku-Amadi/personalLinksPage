8b7358fbcbee0028ba56bc2553fe138882cd3221
Chiazamoku-Amadi <amadichiaza98@gmail.com>
2022-11-05 06:59:35 +0100

    refactor: Moved js files of all components, except Footer, to App.js and made them a sub-route of Landing with path "/" so they are still rendered first; Created a new component - Contact - with js and css files and also made it a sub-route of Landing; Imported Outlet component from react-dom into Landing.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./Components/Landing/Landing";
+import Profile from "./Components/Profile/Profile";
+import Share from "./Components/Share/Share";
+import ShareMobile from "./Components/ShareMobile/ShareMobile";
+import Links from "./Components/Links/Links";
+import Social from "./Components/Social/Social";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Landing />}>
+        <Route
+          path="/"
+          element={
+            <>
+              <section className="profile-section">
+                <Profile />
+              </section>
+              <div className="share-btn">
+                <Share />
+              </div>
+              <div className="share-btn-mobile">
+                <ShareMobile />
+              </div>
+              <section className="links-section">
+                <Links />
+              </section>
+              <section className="social-section">
+                <Social />
+              </section>
+            </>
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,30 +1,12 @@
 import React from "react";
-import Profile from "../Profile/Profile";
-import Share from "../Share/Share";
-import ShareMobile from "../ShareMobile/ShareMobile";
-import Links from "../Links/Links";
-import Social from "../Social/Social";
+import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 
 function Landing() {
   return (
     <div>
       <div className="container">
-        <section className="profile-section">
-          <Profile />
-        </section>
-        <div className="share-btn">
-          <Share />
-        </div>
-        <div className="share-btn-mobile">
-          <ShareMobile />
-        </div>
-        <section className="links-section">
-          <Links />
-        </section>
-        <section className="social-section">
-          <Social />
-        </section>
+        <Outlet />
         <footer className="footer-section">
           <Footer />
         </footer>
